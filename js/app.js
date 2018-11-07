081c5cfefecf23858c91fc1b330e5a9086d87ad1
Shun Git <kwokshungit@gmail.com>
2018-11-07 15:48:19 +0800

    change visual cue for highlight

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@ const size = [1890, 950]
 const svg = d3.select('body').append('svg')
   .attr('width', size[0]).attr('height', size[1])
 
+const terms = [4, 5, 6, 7, 8]
+
+let sections = {}
+terms.forEach((t) => { sections[t] = {} })
+d3.csv('sections.csv', (r) => r.forEach((n) => {
+  terms.filter((t) => n[t]).forEach((t) => { sections[t][n.n] = n[t] })
+}))
+
 let data = []
 
 d3.csv('data.csv', (r) => {
@@ -34,16 +42,16 @@ function seededrandom (seed = 123456) {
 }
 
 function process(data, seed=1) {
-  let times = ['t4', 't5', 't6', 't7', 't8']
   let people = {}, groups = {}
 
   let random = seededrandom(seed)
   let rand = () => random() - 0.5
 
-  times.forEach((t) => { groups[t] = {} })
+  terms.forEach((t) => { groups[t] = {} })
 
   data.forEach((p) => {
-    times.forEach((t) => {
+    p.id = parseInt(p.id)
+    terms.forEach((t) => {
       let k = p[t]
       if (k == '-') return
       if (k in groups[t]) {
@@ -53,22 +61,20 @@ function process(data, seed=1) {
       }
     })
     p.pathed = 0
-    p.width = svg.append('text').attr('class', 'temp')
-      .text(p.nick).node().getComputedTextLength() + 10
     people[p.id] = p
   })
-
-  svg.selectAll('text.temp').remove()
 
   let scenes = [{
     id: 'freshmore',
     characters: Object.values(people).sort(rand).sort((a, b) => a.f - b.f)
   }]
 
-  times.forEach((t) => scenes.push(
-    ...Object.keys(groups[t]).map((k) => ({
-      id: `${t}-${k}`.toUpperCase(),
-      characters: groups[t][k].map((i) => people[i]).sort(rand)
+  terms.forEach((term) => scenes.push(
+    ...Object.keys(groups[term]).map((section) => ({
+      term, section,
+      id: `T${term}-S${section}`,
+      name: sections[term][section],
+      characters: groups[term][section].map((i) => people[i]).sort(rand)
     })).sort(rand)
   ))
 
@@ -81,7 +87,7 @@ function createChart(scenes) {
     .labelSize([50, label]).labelPosition('left')
     .layout()
 
-  let transf = (d) => `translate(${d.x},${d.y})`
+  let transf = (d) => `translate(${Math.round(d.x)},${Math.round(d.y)})`
   let pather = (s) => (d) => {
     let c = d.character
     if (s === null) {
@@ -90,18 +96,24 @@ function createChart(scenes) {
       c.pathed = s
     }
     let t = c.pathed
-    d3.selectAll(`[p='${c.id}']`).style('stroke-opacity', t ? 1 : 0.2)
-    d3.selectAll(`[sp='${c.id}']`)
-      .style('fill', t ? '#666' : '#fff')
-      .attr('r', t ? 3 : 2)
+    d3.selectAll(`[p='${c.id}']`)
+      .style('stroke-width', t ? 2 : 1)
+      .style('stroke-opacity', t ? 1 : 0.2)
+    scenes.forEach((k) => {
+      t = k.characters.some((p) => p.pathed)
+      d3.selectAll(`[s='${k.id}']`)
+        .style('fill-opacity', t ? 1 : 0.7)
+        .style('stroke-width', t ? 2 : 0.5)
+    })
   }
 
-  let drag = d3.behavior.drag().on('drag', function (d) {
+  let drawLinks = (i) => d3.selectAll(`[to='${i}'],[from='${i}']`).attr('d', narrative.link())
+
+  let dragScene = d3.behavior.drag().on('drag', function (d) {
     d.x += d3.event.dx
     d.y += d3.event.dy
     d3.select(this).attr('transform', transf)
-    d3.selectAll(`[from='${d.id}']`).attr('d', narrative.link())
-    d3.selectAll(`[to='${d.id}']`).attr('d', narrative.link())
+    drawLinks(d.id)
   })
 
   d3.selectAll('svg > *').remove()
@@ -112,28 +124,30 @@ function createChart(scenes) {
     .attr('to',(d) => d.target.scene.id)
     .attr('from', (d) => d.source.scene ? d.source.scene.id : d.character.id)
     .style('stroke-opacity', 0.2)
+    .style('stroke-width', 1)
     .on('click', pather(null))
     .on('mouseout', pather(0))
     .on('mouseover', pather(1))
 
   svg.selectAll('.scene').data(narrative.scenes()).enter().call((s) => {
-    let g = s.append('g').call(drag).attr('transform', transf)
+    let g = s.append('g')
+      .call(dragScene)
       .attr('class', 'scene')
+      .attr('transform', transf)
+      .attr('term', (d) => d.term)
+      .attr('section', (d) => d.section)
+      .attr('name', (d) => d.name || d.id)
 
-    g.append('title').text((d) => d.id)
+    g.append('title').text((d) => d.name ? `T${d.term}: ${d.name}` : d.id)
 
     g.append('rect')
+      .attr('s', (d) => d.id)
       .attr('width', 20).attr('height', (d) => d.height)
       .attr('x', -10).attr('y', 0)
       .attr('rx', 3).attr('ry', 3)
+      .style('stroke-width', 0.5)
+      .style('fill-opacity', 0.7)
   })
-
-  svg.selectAll('.scene').selectAll('.appearance').data((d) => d.appearances).enter()
-    .append('circle')
-    .attr('cx', (d) => d.x).attr('cy', (d) => d.y)
-    .attr('sp', (d) => d.character.id)
-    .style('fill', '#fff')
-    .attr('r', 2)
 
   svg.selectAll('.intro').data(narrative.introductions()).enter().append('text')
     .attr('transform', transf)
