5eefe64420eedcc8daeaa8425c668214494b90c9
Shun Git <kwokshungit@gmail.com>
2018-11-12 20:14:08 +0800

    prepare for force layout

--- js/app.js
+++ js/app.js
@@ -1,17 +1,11 @@
-const label = 11
-const size = [1890, 950]
+const size = [window.innerWidth - 20, window.innerHeight - 30]
 const svg = d3.select('body').append('svg')
-  .attr('width', size[0]).attr('height', size[1])
+  .attr('width', size[0])
+  .attr('height', size[1])
 
-const terms = [4, 5, 6, 7, 8]
-const data = {
-  people: {},
-  narrative: null,
-  sections: terms.reduce((o, k) => ({...o, [k]: {}}), {})
-}
+const terms = [3, 4, 5, 6, 7, 8]
 
 window.onhashchange = () => {
-  layoutChart(data.narrative, getSeed())
 }
 
 function reload() {
@@ -32,169 +26,71 @@ function seededrandom (seed = 123456) {
     return (seed - 1) / 2147483646
   }
 }
+class Person {
+  constructor(person) {
+    Object.assign(this, person)
+    this.nodes = []
+    this.selected = 0
+  }
+}
+
+class Group {
+  constructor(group) {
+    Object.assign(this, group)
+    this.nodes = []
+    this.selected = 0
+  }
+}
+
+let data
 
 Promise.all([d3.csv('sections.csv'), d3.csv('data.csv')]).then((r) => {
-  let sections = data.sections
+  let sections = terms.reduce((o, k) => ({...o, [k]: {}}), {})
 
   r[0].forEach((n) => terms.filter((t) => n[t]).forEach((t) => {
     sections[t][n.n] = { name: n[t], people: [] }
   }))
 
-  const raw = r[1].map((p) => ({...p, id: parseInt(p.id)}))
-  const getCommon = (a, b) => terms.reduce((s, t) => s + (a[t] === b[t]), 0)
+  data = process(r[1].map((p) => ({...p, id: parseInt(p.id)})), sections)
+})
 
-  data.people = raw.reduce((o, p) => {
-    const m = raw.filter((q) => p.id !== q.id).map((q) => [getCommon(p, q), q.id]).sort()
-    const common = [...Array(m[m.length-1][0]+1)].map((_, i) => m.filter((c) => c[0] === i).map((c) => c[1]).sort())
-
+function process(raw, sections) {
+  const people = raw.reduce((o, p) => {
     terms.forEach((t) => {
       const k = p[t]
       if (k == '-') return
       sections[t][k].people.push(p.id)
     })
-    return {
-      ...o,
-      [p.id]: {
-        ...p,
-        common,
-        selected: 0,
-        mostCommon: common[common.length-1]
-      }
-    }
+    return {...o, [p.id]: new Person(p)}
   }, {})
 
-  const scenes = [].concat(...terms.map((term) =>
-    Object.keys(sections[term]).map((section) => ({
-      term, section,
-      id: `T${term}-S${section}`,
-      name: sections[term][section].name,
-      characters: sections[term][section].people.map((i) => data.people[i])
+  const groups = [].concat(...terms.map(
+    (term) => Object.keys(sections[term])
+      .map((s) => parseInt(s))
+      .map((section) => new Group({
+        term, section,
+        ...sections[term][section],
+        id: term === 3 ? `F0${section}` : `T${term}-S${section}`,
+      }))
+  ))
+
+  const nodes = [].concat(...groups.map((group) => group.people.map((p) => {
+    const person = people[p], n = {
+      group, person,
+      id: `${group.id}-${people[p].id}`
+    }
+    group.nodes.push(n)
+    person.nodes.push(n)
+    return n
+  })))
+
+  const links = [].concat(...Object.values(people).map(
+    (p) => p.nodes.slice(1).map((n, i) => ({
+      person: p.id,
+      target: n.id,
+      source: p.nodes[i].id
     }))
   ))
 
-  scenes.unshift({
-    id: 'freshmore',
-    characters: Object.values(data.people).sort((a, b) => a.f - b.f)
-  })
-
-  data.narrative = new Narrative(scenes)
-  initChart(data.narrative)
-})
-
-function layoutChart(narrative, seed=1) {
-  const random = seededrandom(seed)
-  const rand = () => random() - 0.5
-
-  narrative.characters.sort(rand)
-  narrative.scenes.forEach((s) => {
-    s.characters.sort(rand)
-    if (s.id == 'freshmore') {
-      s.appearances.sort(rand).sort((a, b) => a.character.f - b.character.f)
-    } else {
-      s.appearances.sort(rand)
-    }
-  })
-
-  narrative.size = size
-  narrative.pathSpace = label
-  narrative.labelSize = [50, label]
-  narrative.layout()
-  adjustChart(narrative)
-}
-
-function adjustChart(narrative) {
-  svg.selectAll('.link').attr('d', narrative.link())
-  svg.selectAll('.scene').attr('transform', narrative.transform())
-  svg.selectAll('.intro').attr('transform', narrative.transform())
-  svg.selectAll('.appearance').attr('transform', narrative.transform())
-}
-
-function initChart(narrative) {
-  const showSelections = () => {
-    svg.selectAll('.link')
-      .style('stroke-width', (d) => d.character.selected ? 2 : 1)
-      .style('stroke-opacity', (d) => d.character.selected ? 1 : 0.2)
-
-    svg.selectAll('.appearance')
-      .style('fill', (d) => d.character.selected ? '#666' : '#fff')
-
-    narrative.scenes.forEach((k) => {
-      const h = k.characters.some((p) => p.selected)
-      svg.selectAll(`[s='${k.id}']`)
-        .style('fill-opacity', h ? 1 : 0.7)
-        .style('stroke-width', h ? 2 : 0.5)
-    })
-
-    const selected = narrative.characters.filter((c) => c.selected == 2)
-    if ( selected.length === 0 ) return
-
-    const getCommon = (c) => terms.reduce((v, t) => v + selected.some((s) => s[t] === c[t]), 0)
-    const com = narrative.characters.map((c) => [getCommon(c), c.id]).sort()
-    const max = com[com.length - 1][0]
-    svg.selectAll('.link')
-      .style('stroke-opacity', (d) => com.find((i) => i[1] === d.character.id)[0] / max)
-  }
-
-  const selectLink = (s) => (d) => {
-    const c = d.character
-    if (s === 2) {
-      c.selected = (c.selected === 2) ? 0 : 2
-    } else if ( c.selected !== 2 ){
-      c.selected = s
-    }
-    showSelections()
-  }
-
-  const setTransf = (i) => svg.selectAll(i).attr('transform', narrative.transform())
-  const drawLinks = (i) => svg.selectAll(`[to='${i}'],[from='${i}']`).attr('d', narrative.link())
-
-  const dragScene = d3.drag().on('drag', function (d) {
-    d.x += d3.event.dx
-    d.y += d3.event.dy
-    d3.select(this).attr('transform', narrative.transform())
-    drawLinks(d.id)
-  })
-
-  layoutChart(narrative, getSeed())
-
-  svg.selectAll('.link').data(narrative.links).enter().append('path')
-    .attr('class', 'link')
-    .attr('p', (d) => d.character.id)
-    .attr('to',(d) => d.target.scene.id)
-    .attr('from', (d) => d.source.scene ? d.source.scene.id : d.character.id)
-    .on('click', selectLink(2))
-    .on('mouseout', selectLink(0))
-    .on('mouseover', selectLink(1))
-
-  svg.selectAll('.scene').data(narrative.scenes).enter().call((s) => {
-    const g = s.append('g')
-      .call(dragScene)
-      .attr('class', 'scene')
-      .attr('term', (d) => d.term)
-      .attr('section', (d) => d.section)
-      .attr('name', (d) => d.name || d.id)
-
-    g.append('title').text((d) => d.name ? `T${d.term}: ${d.name}` : d.id)
-
-    g.append('rect')
-      .attr('s', (d) => d.id)
-      .attr('width', 20).attr('x', -10)
-      .attr('height', (d) => d.height)
-      .attr('rx', 3).attr('ry', 3)
-  })
-
-  svg.selectAll('.scene').selectAll('.appearance').data((d) => d.appearances).enter()
-    .append('circle')
-    .attr('class', 'appearance')
-    .attr('p', (d) => d.character.id)
-    .attr('r', 3)
-
-  svg.selectAll('.intro').data(narrative.introductions).enter().append('text')
-    .attr('class', 'intro')
-    .attr('y', 2).attr('x', -4)
-    .attr('text-anchor', 'end')
-    .text((d) => d.character.nick)
-
-  showSelections()
-  adjustChart(narrative)
+  return {people, groups, nodes, links}
 }
